9e732e2f81de3e048c19985faff6e8474e40962c
Wesley <wesmith314@gmail.com>
2023-09-07 14:28:58 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Hailee Smith'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
